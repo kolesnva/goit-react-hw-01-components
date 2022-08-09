c3fb5b753d3c5632c441d4186707cd10a79c8827
kolesnva <kolesnychenko_v@icloud.com>
2022-08-09 18:59:31 +0300

    finished t-01 body

--- src/components/task-01-profile/UserInfo.js
+++ src/components/task-01-profile/UserInfo.js
@@ -1,20 +1,52 @@
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
-
-import { UserImage } from './UserInfoStyled';
-import { UserName } from './UserInfoStyled';
-import { UserTag } from './UserInfoStyled';
-import { UserLocation } from './UserInfoStyled';
+import {
+  UserImage,
+  UserName,
+  UserTag,
+  UserLocation,
+  UserStatsList,
+  StatItem,
+  ItemSpan,
+} from './UserInfoStyled';
 
 export function UserInfo(user) {
   const { username, tag, location, avatar, stats } = user;
 
   return (
-    <Box>
-      <UserImage src={avatar} alt="user avatar" className="avatar" />
-      <UserName className="name">{username}</UserName>
-      <UserTag>{`@` + tag}</UserTag>
-      <UserLocation>{location}</UserLocation>
+    <Box className="profile">
+      <Box className="description">
+        <UserImage src={avatar} alt="user avatar" className="avatar" />
+        <UserName className="name">{username}</UserName>
+        <UserTag className="tag">{`@` + tag}</UserTag>
+        <UserLocation className="location">{location}</UserLocation>
+      </Box>
+      <UserStatsList className="stats">
+        <StatItem>
+          <ItemSpan className="label">Followers</ItemSpan>
+          <ItemSpan className="quantity">{stats.followers}</ItemSpan>
+        </StatItem>
+        <StatItem>
+          <ItemSpan className="label">Views</ItemSpan>
+          <ItemSpan className="quantity">{stats.views}</ItemSpan>
+        </StatItem>
+        <StatItem>
+          <ItemSpan className="label">Likes</ItemSpan>
+          <ItemSpan className="quantity">{stats.likes}</ItemSpan>
+        </StatItem>
+      </UserStatsList>
     </Box>
   );
 }
+
+UserInfo.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
--- src/components/task-01-profile/UserInfoStyled.js
+++ src/components/task-01-profile/UserInfoStyled.js
@@ -1,15 +1,9 @@
 import styled from 'styled-components';
 
 export const UserImage = styled.img``;
-
 export const UserName = styled.p``;
-
 export const UserTag = styled.p``;
-
 export const UserLocation = styled.p``;
-
-export const UserStats = styled.ul``;
-
-export const StatPoint = styled.li``;
-
-export const PointSpan = styled.span``;
+export const UserStatsList = styled.ul``;
+export const StatItem = styled.li``;
+export const ItemSpan = styled.span``;
